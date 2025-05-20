f18a2d115d8b896ba81afaa677297efb3247749f
throgg <ghus266@gmail.com>
2025-05-20 19:20:37 +0200

    added documentation

--- fluid_movement.jl
+++ fluid_movement.jl
@@ -21,7 +21,7 @@ begin
 	using Pkg;
 	Pkg.activate(".");
 	Pkg.status();
-	using GLMakie, PlutoUI;
+	using GLMakie, PlutoUI, GeoStats, Random;
 end
 
 # ╔═╡ 7c88cee5-b895-42b5-9df4-94bcb4425574
@@ -54,110 +54,68 @@ for i in range(1, nx-1):
 $(@bind t_view PlutoUI.Slider(1:nt_, show_value=true))
 """
 
-# ╔═╡ e61c7792-6431-4438-83b0-a56ed1b772eb
+# ╔═╡ 9534870e-2b83-4590-8e3b-030f63816c43
 md"""
-## 2D dispersion equation
+## 2D convection equation
+Convection only occurs in fluids and can be expressed with [this](https://drzgan.github.io/Python_CFD/9.%202D%20nonlinear%20convection.html) equation :
 
-## Convection-Diffusion
-A mixing of both terms is presented [here](https://louis-finegan.github.io/Convection-Diffusion-Pages/pages/Two_Dimensional_Models.html).
+X momentum equation : 
+
+$$\frac{\partial u}{\partial t} + u \frac{\partial u}{\partial x} + v \frac{\partial u}{\partial y} = -\frac{1}{\rho_0}\frac{\partial P}{\partial x}+ \nu (\frac{\partial^2u}{\partial x^2} + \frac{\partial^2u}{\partial y^2})$$
+
+Y momentum equation :
+
+$$\frac{\partial v}{\partial t} + u \frac{\partial v}{\partial x} + v \frac{\partial v}{\partial y} = -\frac{1}{\rho_0}\frac{\partial P}{\partial y}+ \nu (\frac{\partial^2v}{\partial x^2} + \frac{\partial^2v}{\partial y^2}) - g \beta (T-T_{ref})$$
+
+[Source](https://scientiairanica.sharif.edu/article_4022_4c02163d61a0e092d1b0b0519dd23f4e.pdf)
+
+Where : 
+
+| | |
+|---|---|
+|u|x mometum|
+|v|y momentum|
+|P|Pressure in Pa|
+|$g \beta (T-T_{ref})$|Buoyancy term, makes the fluid rise due to differences in density|
+|$\nu (\frac{\partial^2v}{\partial x^2}$|Viscous advection terms|
+|T|Temperature (in Kelvin)|
+|t|time (seconds)|
+|ν|kinematic viscosity|
+|g|acceleration due to gravity $m.s²$|
+|β|expansion coefficient|
+
+And distances in meters.
+
+## Advection diffusion for temperature
+
+$$\frac{\partial T}{\partial t}+u\frac{\partial T}{\partial x} + v\frac{\partial T}{\partial y} = \alpha (\frac{\partial^2T}{\partial x^2}+\frac{\partial^2 T}{\partial y^2})$$
+Where terms on the right is the thermal diffusion. 
+
+[Source](https://www3.nd.edu/~powers/ame.60614/hwex.2004/hw4.pdf)
+## Discretization
+Uses finite differences, with forward for time and central for spatial derivatives. 
+
+$$u_{i,j}^{n+1} = u_{i,j}^n + \Delta t \left[ - \left( u_{i,j}^n \frac{u_{i+1,j}^n - u_{i-1,j}^n}{2 \Delta x} + v_{i,j}^n \frac{u_{i,j+1}^n - u_{i,j-1}^n}{2 \Delta y} \right) + \nu \left( \frac{u_{i+1,j}^n - 2u_{i,j}^n + u_{i-1,j}^n}{(\Delta x)^2} + \frac{u_{i,j+1}^n - 2u_{i,j}^n + u_{i,j-1}^n}{(\Delta y)^2} \right) \right]$$
+
+$v_{i,j}^{n+1} = v_{i,j}^n + \Delta t \left[ - \left( u_{i,j}^n \frac{v_{i+1,j}^n - v_{i-1,j}^n}{2 \Delta x} + v_{i,j}^n \frac{v_{i,j+1}^n - v_{i,j-1}^n}{2 \Delta y} \right) + \nu \left( \frac{v_{i+1,j}^n - 2v_{i,j}^n + v_{i-1,j}^n}{(\Delta x)^2} + \frac{v_{i,j+1}^n - 2v_{i,j}^n + v_{i,j-1}^n}{(\Delta y)^2} \right) + g \beta (T_{i,j}^n - T_{ref}) \right]$
+
+$$T_{i,j}^{n+1} = T_{i,j}^n + \Delta t \left[ - \left( u_{i,j}^n \frac{T_{i+1,j}^n - T_{i-1,j}^n}{2 \Delta x} + v_{i,j}^n \frac{T_{i,j+1}^n - T_{i,j-1}^n}{2 \Delta y} \right) + \alpha \left( \frac{T_{i+1,j}^n - 2T_{i,j}^n + T_{i-1,j}^n}{(\Delta x)^2} + \frac{T_{i,j+1}^n - 2T_{i,j}^n + T_{i,j-1}^n}{(\Delta y)^2} \right) \right]$$
 """
 
-# ╔═╡ 59b99db1-7852-40eb-b02a-8d3b6f79fcca
-@bind taa PlutoUI.Slider(1:500)
-
-# ╔═╡ c30bde2d-e0df-4fb3-b5b1-11455a8364f2
-begin
-	# --- Simulation parameters ---
-	nx = 51  # Number of grid points in x
-	ny = 51  # Number of grid points in y
-	nt = 500 # Number of time steps
-	lx = 5.0  			# Length in x
-	ly = 5.0  			# Length in y
-	dx = lx / (nx - 1)
-	dy = ly / (ny - 1)
-		
-	dt = 0.001 # Time step size
-	Pr = 7.01  # Prandtl number (for water)
-	Ra = 1e4   # Rayleigh number (controls convection strength)
-	
-	T_res = 91
-	T_inj = 90
-		
-	g = 9.81 # Acceleration due to gravity
-	β = 1.0  # Thermal expansion coefficient (simplified)
-	ν = sqrt(g * β * abs(T_res - T_inj) * ny^3 / Ra) / sqrt(Ra * Pr)
-	α = ν / Pr # Thermal diffusivity
-	
-	uc = Array{Float64, 3}(undef, nx, ny, nt); fill!(uc, 0.0)
-	vc = Array{Float64, 3}(undef, nx, ny, nt); fill!(vc, 0.0)
-	T = Array{Float64, 3}(undef, nx, ny, nt); fill!(T, (T_inj + T_res) / 2)
-	p = Array{Float64, 3}(undef, nx, ny, nt); fill!(p, 0.0) # Pressure
-	
-	# Initial conditions
-	T[15:20, 15:20, 1:10] .= T_inj
-	
-	
-	# Time-stepping loop
-	for t in 1:(nt - 1)
-	    for i in 2:(nx - 1)
-	        for j in 2:(ny - 1)
-	    		# Spatial derivatives (central difference for now)
-	            duc_dx = (uc[i+1, j, t] - uc[i-1, j, t]) / (2 * dx)
-	            duc_dy = (uc[i, j+1, t] - uc[i, j-1, t]) / (2 * dy)
-	            dvc_dx = (vc[i+1, j, t] - vc[i-1, j, t]) / (2 * dx)
-	            dvc_dy = (vc[i, j+1, t] - vc[i, j-1, t]) / (2 * dy)
-					
-	            dT_dx = (T[i+1, j, t] - T[i-1, j, t]) / (2 * dx)
-	            dT_dy = (T[i, j+1, t] - T[i, j-1, t]) / (2 * dy)
-	
-	            d2uc_dx2 = (uc[i+1, j, t] - 2 * uc[i, j, t] + uc[i-1, j, t]) / dx^2
-	            d2uc_dy2 = (uc[i, j+1, t] - 2 * uc[i, j, t] + uc[i, j-1, t]) / dy^2
-	            d2vc_dx2 = (vc[i+1, j, t] - 2 * vc[i, j, t] + vc[i-1, j, t]) / dx^2
-	            d2vc_dy2 = (vc[i, j+1, t] - 2 * vc[i, j, t] + vc[i, j-1, t]) / dy^2
-	            d2T_dx2 = (T[i+1, j, t] - 2 * T[i, j, t] + T[i-1, j, t]) / dx^2
-	            d2T_dy2 = (T[i, j+1, t] - 2 * T[i, j, t] + T[i, j-1, t]) / dy^2
-	
-	            # Momentum equations (x and y) with Boussinesq approximation
-	            duc_dt = -(uc[i,j,t]*duc_dx+vc[i,j,t]*duc_dy)+ν*(d2uc_dx2+d2uc_dy2)
-	            dvc_dt = -(uc[i,j,t]*dvc_dx+vc[i, j, t]*dvc_dy)+ν*(d2vc_dx2 + d2vc_dy2) +g*β*(T[i,j,t]-(T_inj+T_res)/2)
-	            dT_dt = -(uc[i,j,t]*dT_dx+vc[i,j,t]*dT_dy)+α*(d2T_dx2+d2T_dy2)
-	
-	            # Update values
-	            uc[i, j, t+1] = uc[i, j, t] + dt * duc_dt
-	            vc[i, j, t+1] = vc[i, j, t] + dt * dvc_dt
-	            T[i, j, t+1] = T[i, j, t] + dt * dT_dt
-	    	end
-	    end
-	
-	    # Boundary conditions (simplified no-slip and fixed temperature)
-		# UC boundaries (no velocity on edges)
-	    uc[:, 1, t+1].= 0.0
-		uc[:, ny, t+1].=0.0
-		uc[1, :, t+1].= 0.0
-		uc[nx, :, t+1] .= 0.0
-		# VC boundaries (no velocity on edges)
-	    vc[:, 1, t+1] .= 0.0
-		vc[:, ny, t+1] .= 0.0
-		vc[1, :, t+1] .= 0.0
-		vc[nx, :, t+1] .= 0.0
-		# Temperature boundary conditions
-		avg = (T_res + T_inj) / 2
-	    T[:, 1, t+1] .= avg
-		T[:, ny, t+1] .= avg
-		T[1, :, t+1]  .= avg
-		T[nx, :, t+1] .= avg
-	    #T[1, :, t+1] .= (T_res + T_inj) / 2 # side boundary
-	    #T[nx, :, t+1] .= (T_res + T_inj) / 2 # side boundary
-	end
-	temperature_field=T
-
-	# plotting 
-	c_range = (T_inj, T_res) # color range
-	
-	fig, ax, hm = heatmap(temperature_field[:, :, taa], colorrange=c_range)
-	Colorbar(fig[:, end+1], colorrange=c_range)
-	fig
-end
+# ╔═╡ 214f2271-730f-4236-9761-a574295f12bf
+md"""
+## Input parameters
+|Parameter|Value|
+|---|---|
+|Nx|$(@bind nx PlutoUI.Slider(10:200, default=31, show_value=true))|
+|Ny|$(@bind ny PlutoUI.Slider(10:200, default=31, show_value=true))|
+|Nt|$(@bind nt PlutoUI.Slider(10:5000, default=500, show_value=true))|
+|lx|$(@bind lx PlutoUI.Slider(10:10:1000, default=10.0, show_value=true))|
+|ly|$(@bind ly PlutoUI.Slider(10:10:200, default=10.0, show_value=true))|
+|$T_{res}$|$(@bind T_res_ PlutoUI.Slider(10:200, default=120, show_value=true))|
+|$T_{inj}$|$(@bind T_inj_ PlutoUI.Slider(10:200, default=90, show_value=true))|
+|Rayleigh|$(@bind Ra_ PlutoUI.NumberField(1:6, default=1e4))
+"""
 
 # ╔═╡ 909d507c-972e-4ce3-a8c3-3965823b0e2b
 begin
@@ -185,35 +143,142 @@ begin
 	heatmap(ud[:, :, t_view])
 end
 
-# ╔═╡ 9534870e-2b83-4590-8e3b-030f63816c43
+# ╔═╡ 3be91d09-7b8b-4fd7-8719-6c6ce9e16a8f
 md"""
-## 2D convection equation
-Convection only occurs in fluids and can be expressed with [this](https://drzgan.github.io/Python_CFD/9.%202D%20nonlinear%20convection.html) equation :
+## Permeability
+Rayleigh number can be linked to permeability with Raleigh-Darcy number ([source](https://web.mit.edu/1.63/www/Lec-notes/chap6_porous/6-6Lapwood.pdf)) : 
 
-$$\frac{\partial u}{\partial t} + u \frac{\partial u}{\partial x} + v \frac{\partial u}{\partial y} = 0$$
+$$Ra = \frac{\rho \beta \Delta T klg}{n\alpha}$$
+With : 
+- k the permeability of the porous medium ✓
+- ρ density of the fluid ✓
+- ΔT difference in temperature between T₀ and T₀+ΔT ≈ Tres-Tinj ✓
+- β expansion coefficient ✓
+- l characteristic length ≈ height of reservoir in meters ✓
+- ν kinematic viscosity ✓
+- α thermal diffusivity ✓
 
-$$\frac{\partial v}{\partial t} + u \frac{\partial v}{\partial x} + v \frac{\partial v}{\partial y} = 0$$
-
-Where are the u and v component of velocity (x velocity and y velocity)
-
-### Discretization 
-
-$$u_{i,j}^{n+1} = u_{i,j}^n - u_{i,j} \frac{\Delta t}{\Delta x} (u_{i,j}^n-u_{i-1,j}^n) - v_{i,j}^n \frac{\Delta t}{\Delta y} (u_{i,j}^n-u_{i,j-1}^n)$$ 
-
-And v: 
-
-$$v_{i,j}^{n+1} = v_{i,j}^n - u_{i,j} \frac{\Delta t}{\Delta x} (v_{i,j}^n-v_{i-1,j}^n) - v_{i,j}^n \frac{\Delta t}{\Delta y} (v_{i,j}^n-v_{i,j-1}^n)$$
-
-### Code implementation
-$(@bind t_step PlutoUI.Slider(1:1:nt, show_value=true))
 """
 
+# ╔═╡ c30bde2d-e0df-4fb3-b5b1-11455a8364f2
+begin
+	T_inj = T_inj_ + 273
+	T_res = T_res_ + 273
+	dx = lx / (nx - 1)
+	dy = ly / (ny - 1)
+		
+	dt = 0.1 # Time step size
+	Pr = 7.01  # Prandtl number (for water)
+	Ra = 1e4 # Rayleigh number (controls convection strength)
+		
+	g = 9.81 # Acceleration due to gravity
+	β = 0.75e-3  # Thermal expansion coefficient (water @ 100°C)
+	# TODO: change to not use Ra
+	ν_water = 2.938e-7
+	ν = sqrt(g * β * abs(T_res - T_inj) * ny^3 / Ra) / sqrt(Ra * Pr)
+	α = ν_water / Pr 		# Thermal diffusivity ~ 0.02 
+	
+	ρ_water = 1000 	# kg.m³
+	# array of x velocities
+	uc = Array{Float64, 3}(undef, nx, ny, nt)
+	fill!(uc, 0.0)
+	# array of y velocities
+	vc = Array{Float64, 3}(undef, nx, ny, nt)
+	fill!(vc, 0.0)
+	# temperature array
+	T = Array{Float64, 3}(undef, nx, ny, nt)
+	fill!(T, (T_inj + T_res) / 2)
+	# pressure array
+	p = Array{Float64, 3}(undef, nx, ny, nt)
+	fill!(p, 0.0)
+	ΔT = abs(T_inj-T_res)
+	# Initial conditions
+	x_offset, y_offset = 10, 2
+	T[(nx÷2)-x_offset:(nx÷2)+x_offset, (ny÷2)-y_offset:(ny÷4)+y_offset, :] .= T_inj
+	
+	for t in 1:(nt - 1) # time step 
+		for coords ∈ CartesianIndices((2:(nx - 1), 2:(ny - 1)))
+			i = coords[1]
+			j = coords[2]
+			
+			# Ra_Da = DarcyRayleigh(ρ_water, β, ΔT, k, ly, ν, α)
+			# α = alpha(β, ΔT, Ra_Da, Pr, g)
+	    	# Spatial derivatives (central differences)
+	        duc_dx = (uc[i+1, j, t] - uc[i-1, j, t]) / (2 * dx)
+	        duc_dy = (uc[i, j+1, t] - uc[i, j-1, t]) / (2 * dy)
+	        dvc_dx = (vc[i+1, j, t] - vc[i-1, j, t]) / (2 * dx)
+	        dvc_dy = (vc[i, j+1, t] - vc[i, j-1, t]) / (2 * dy)
+
+	        dT_dx = (T[i+1, j, t] - T[i-1, j, t]) / (2 * dx) # ΔT/Δy
+	        dT_dy = (T[i, j+1, t] - T[i, j-1, t]) / (2 * dy) # ΔT/Δy
+	
+	    	d2uc_dx2 = (uc[i+1, j, t] - 2 * uc[i, j, t] + uc[i-1, j, t]) / dx^2
+	        d2uc_dy2 = (uc[i, j+1, t] - 2 * uc[i, j, t] + uc[i, j-1, t]) / dy^2
+	        d2vc_dx2 = (vc[i+1, j, t] - 2 * vc[i, j, t] + vc[i-1, j, t]) / dx^2
+	        d2vc_dy2 = (vc[i, j+1, t] - 2 * vc[i, j, t] + vc[i, j-1, t]) / dy^2
+	        d2T_dx2 = (T[i+1, j, t] - 2 * T[i, j, t] + T[i-1, j, t]) / dx^2
+	        d2T_dy2 = (T[i, j+1, t] - 2 * T[i, j, t] + T[i, j-1, t]) / dy^2
+	
+	        # Momentum equations (x and y) with Boussinesq approximation
+	        duc_dt = -(uc[i,j,t]*duc_dx+vc[i,j,t]*duc_dy)+ν*(d2uc_dx2+d2uc_dy2)*0.01
+	        dvc_dt = -(uc[i,j,t]*dvc_dx+vc[i, j, t]*dvc_dy)+ν*(d2vc_dx2 + d2vc_dy2) +g*β*(T[i,j,t]-(T_inj+T_res)/2)*0.02
+	        dT_dt = -(uc[i,j,t]*dT_dx+vc[i,j,t]*dT_dy)+α*(d2T_dx2+d2T_dy2)
+	
+	        # Update values
+	        uc[i, j, t+1] = uc[i, j, t] + dt * duc_dt
+	        vc[i, j, t+1] = vc[i, j, t] + dt * dvc_dt
+	        T[i, j, t+1] = T[i, j, t] + dt * dT_dt
+	    	# end
+	    end
+	
+	    # Boundary conditions (simplified no-slip and fixed temperature)
+		# UC boundaries (no velocity on edges)
+	    uc[:, 1, t+1].= 0.0
+		uc[:, ny, t+1].= 0.0
+		uc[1, :, t+1].= 0.0
+		uc[nx, :, t+1] .= 0.0
+		# VC boundaries (no velocity on edges)
+	    vc[:, 1, t+1] .= 0.0
+		vc[:, ny, t+1] .= 0.0
+		vc[1, :, t+1] .= 0.0
+		vc[nx, :, t+1] .= 0.0
+		# Temperature boundary conditions
+		# T[15:20, 15:20, t+1] .= T_inj
+		T[(nx÷2)-x_offset:(nx÷2)+x_offset, (ny÷2)-y_offset:(ny÷2)+y_offset, t+1] .= T_inj
+		avg = (T_res + T_inj) / 2
+	    
+		T[:, ny, t+1] .= avg # top boundary
+		T[1, :, t+1]  .= avg # side boundaries
+		T[nx, :, t+1] .= avg # side boundaries
+		T[:, 1, t+1]  .= avg # bottom boundary
+	    #T[1, :, t+1] .= (T_res + T_inj) / 2 # side boundary
+	    #T[nx, :, t+1] .= (T_res + T_inj) / 2 # side boundary
+	end
+	temperature_field=T
+	print()
+end
+
+# ╔═╡ 59b99db1-7852-40eb-b02a-8d3b6f79fcca
+@bind taa PlutoUI.Slider(1:nt, show_value=true)
+
+# ╔═╡ 0a72c6aa-0602-44b2-85a3-d68401aa7c31
+begin
+	# plotting 
+	c_range = (T_inj, T_res) # color range
+	
+	fig, ax, hm = heatmap(temperature_field[:, :, taa], colorrange=c_range)
+	Colorbar(fig[:, end+1], colorrange=c_range)
+	fig
+end
+
 # ╔═╡ Cell order:
-# ╟─0a66c416-3484-11f0-0778-a1e104318d7b
+# ╠═0a66c416-3484-11f0-0778-a1e104318d7b
 # ╟─f1d283dc-9b8a-40a1-a947-f29588b98c2f
 # ╟─7c88cee5-b895-42b5-9df4-94bcb4425574
 # ╟─909d507c-972e-4ce3-a8c3-3965823b0e2b
-# ╠═9534870e-2b83-4590-8e3b-030f63816c43
-# ╟─e61c7792-6431-4438-83b0-a56ed1b772eb
-# ╠═59b99db1-7852-40eb-b02a-8d3b6f79fcca
+# ╟─9534870e-2b83-4590-8e3b-030f63816c43
+# ╠═214f2271-730f-4236-9761-a574295f12bf
+# ╟─3be91d09-7b8b-4fd7-8719-6c6ce9e16a8f
 # ╠═c30bde2d-e0df-4fb3-b5b1-11455a8364f2
+# ╟─59b99db1-7852-40eb-b02a-8d3b6f79fcca
+# ╠═0a72c6aa-0602-44b2-85a3-d68401aa7c31
